70d05e0444578f69bfc349bb46e96375cbb57d41
felit <nofelit@126.com>
2017-03-01 20:18:02 +0800

    添加mytest module

--- modules/demo/setup.py
+++ modules/demo/setup.py
@@ -1,1 +1,14 @@
-__author__ = 'congsl'
+# -*- coding:utf8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="mytest",
+    version="0.10",
+    description="test module",
+    author="jack cong",
+    url="http://www.livedrof.com",
+    license="LGPL",
+    packages=find_packages(),
+    scripts=["mytest/test.py"],
+    py_modules=['mytest']
+)
